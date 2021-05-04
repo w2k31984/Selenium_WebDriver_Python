1ca7062c1ad425889fd2343b8ebd5ed757da88cc
Cristian Parada <cmpmendoza12@gmail.com>
2021-05-04 10:33:29 -0600

    Explicacion de operadores en python

---
+++ Operadores.py
@@ -0,0 +1,19 @@
+#Aritmeticos
+suma = 2+5
+print(suma)
+
+resta= 3-2
+print(resta)
+
+multiplicacion= 5*5
+print(multiplicacion)
+
+division= 25/5
+print(division)
+
+exponente= 5**3
+print(exponente)
+
+#El residio es lo que queda del resultado de una division en el ejemplo 25/2= 6 porque 6*2= 24 queda 1 para 25.
+residuo= 25%2
+print(residuo)
