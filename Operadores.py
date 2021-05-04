bc57b3fb782e97f66535216bf0be3b8be5366436
Cristian Parada <cmpmendoza12@gmail.com>
2021-05-04 12:22:34 -0600

    Agregacion de Ejercicio de Calendario utilizando libreria calendar, y explicacion de operadores basicos en python

--- Operadores.py
+++ Operadores.py
@@ -17,3 +17,54 @@ print(exponente)
 #El residio es lo que queda del resultado de una division en el ejemplo 25/2= 6 porque 6*2= 24 queda 1 para 25.
 residuo= 25%2
 print(residuo)
+
+#Relacionales para comparaciones o declaraciones
+
+#= asignando valor a una variable
+a= 3
+print(a)
+
+#== comparacion comparando dos valores
+a=2
+b=3
+print (a==b)
+
+#!= No es igual esto es para saber que un dato es diferente a otro.
+print(a!=b)
+
+#> Mayor que para verificar si un dato es mayor
+print(a>b)
+
+#< Menor que para verificar si un datos es menor.
+print(a<b)
+
+#>= Mayor o igual que.
+print(a>=b)
+
+#<= Menor o igual que.
+print(a<=b)
+
+#Boleanos
+#False
+print(False)
+
+#True
+print(True)
+
+#Not nos ayuda a negar
+print(not False)
+print(not True)
+
+
+#Is verificar si un elemento es igual a otro.
+
+#Logicos
+#And Ayuda a comparar multiples valores en una sola linea de codigo para que de verdadero todas las operaciones deben ser iguales.
+print(1 == 1 and 2==2 and 2==4)
+
+#Or Solo se tiene que cumplir una de las condiciones para que de verdadero es menos estricto que AND
+print(2==1 or 1==1 or 3==2 or 2==4)
+
+
+
+
---
+++ calendario.py
@@ -0,0 +1,4 @@
+import calendar
+year = 2021
+month = 5
+print(calendar.month(year,month))
