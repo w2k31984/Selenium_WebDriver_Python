877d18cc3f37cde701db7e114f696bc7cb343650
Cristian Parada <cmpmendoza12@gmail.com>
2021-05-20 16:03:33 -0600

    Creacion de script para correr el navegador en segundo plano.

---
+++ Funciones_Selenium_WebDriver/Ejecucion_Segundo_Plano.py
@@ -0,0 +1,28 @@
+#El navegador estara corriendo en segundo plano y lo monitoriaremos con el monitor de tareas
+import configparser
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+#Agregando argumentos de trabajo
+options = Options()
+options.add_argument('--headless') #Con esta linea abrimos el navegador en 2o Plano
+
+def main():
+    configuracion = configparser.ConfigParser()
+    configuracion.read('../Selenium_WebDriver/config.ini')
+    configuracion.sections()
+
+    webdri = configuracion['General']['chrome']
+    web = configuracion['Paginas']['pagina3']
+
+    driver = webdriver.Chrome(executable_path='../Selenium_WebDriver/chromedriver.exe',chrome_options=options)
+    driver.get(web)
+
+    #saber si estamos en la web nos permite traer el titulo de la pagina actual.
+    print(driver.title)
+
+    input()
+
+if __name__  == '__main__':
+    main()
